d894d60cc2a2d146ceaa5ea041173dfafe8041d2
goperdoor <goperdoor576124@gmail.com>
2025-07-13 13:29:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,16 @@ import './App.css';
 import ContactUs from './Pages/ContactUs'
 import PerdoorPage from './Pages/PerdoorPage'
 import PrivacyPolicy from './Pages/PrivacyPolicy'
+import Header from './components/BusTimingHeader';
+import Footer from './components/Footer';
+import WisdomWall from './Pages/WisdomWall';
+
 
 function App() {
   return (
-    <Router>
+    
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
@@ -19,9 +24,11 @@ function App() {
           <Route path="/ContactUs" element={<ContactUs />} />
           <Route path="/PerdoorPage" element={<PerdoorPage />} />
           <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          <Route path="/WisdomWall" element={<WisdomWall />} />
         </Routes>
+        <Footer />
       </div>
-    </Router>
+
   );
 }
 
