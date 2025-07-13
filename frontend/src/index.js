0465a809901eeb860dffa433b2a145a398fca562
goperdoor <goperdoor576124@gmail.com>
2025-07-13 21:57:11 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ✅ Add this
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter >
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
+
+// ✅ Register the service worker
+serviceWorkerRegistration.register();
