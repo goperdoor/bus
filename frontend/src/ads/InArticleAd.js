b7b639c8ba809a90e827ce8863512e0baf80d3e3
goperdoor <goperdoor576124@gmail.com>
2025-08-12 05:47:24 +0530

    Create InArticleAd.js

---
+++ frontend/src/ads/InArticleAd.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+const InArticleAd = () => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error('AdSense error:', e);
+    }
+  }, []);
+
+  return (
+    <ins className="adsbygoogle"
+      style={{ display: 'block', textAlign: 'center' }}
+      data-ad-layout="in-article"
+      data-ad-format="fluid"
+      data-ad-client="ca-pub-4093862221318795"
+      data-ad-slot="5378504439"
+    />
+  );
+};
+
+export default InArticleAd;
